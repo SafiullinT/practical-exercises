dfa83d5599dd3e9fe691681c04a8ae2f92ea77ba
Timur <64526441+SafiullinT@users.noreply.github.com>
2023-03-05 23:36:30 +0300

    Add files via upload

---
+++ week 3/Ex 2/Задача 2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <locale>
+#include "rational.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите количество дробей: ";
+	cin >> n;
+	rational* arr = new rational[n];
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cout << "Введите числитель и знаменатель дроби " << i + 1 << ": ";
+		cin >> a >> b;
+		arr[i].set(a, b);
+	}
+	for (int i = 0; i < n; i++) {
+		arr[i].show();
+		cout << endl;
+	}
+	delete[] arr;
+	return 0;
+}
---
+++ week 3/Ex 3/Задача 3.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <locale>
+#include "../Задача 2/rational.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите количество дробей(2 дроби и более): ";
+	cin >> n;
+	rational* arr = new rational[n];
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cout << "Введите числитель и знаменатель дроби " << i + 1 << ": ";
+		cin >> a >> b;
+		arr[i].set(a, b);
+	}
+	for (int i = 0; i < n; i++) {
+		arr[i].show();
+		cout << endl;
+	}
+
+	rational sum;
+	for (int i = 0; i < n; i++) {
+		sum = sum + arr[i];
+	}
+	cout << "Сумма дробей: ";
+	sum.show();
+	cout << endl;
+
+	rational diff = arr[0] - arr[1];
+	cout << "Разность дробей: ";
+	diff.show();
+	cout << endl;
+
+	cout << "Инкремент: ";
+	arr[0]++;
+	arr[0].show();
+	cout << endl;
+
+	cout << "Сравнение дробей: ";
+	if (arr[0] > arr[1]) cout << "Первая дробь больше второй.";
+	else if (arr[0] == arr[1]) cout << "Первая дробь равна второй.";
+	else cout << "Первая дробь меньше второй.";
+
+	arr[0] = arr[1];
+	cout << endl << "Присваивание: ";
+	arr[0].show();
+	cout << endl;
+
+	delete[] arr;
+	return 0;
+}
