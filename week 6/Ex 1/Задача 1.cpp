6057bb14b46747549a4269d57bfe336bb7999fd7
Timur <64526441+SafiullinT@users.noreply.github.com>
2023-03-26 23:43:39 +0300

    Add files via upload

---
+++ week 6/Ex 1/Задача 1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <cmath>
+#include "Cone.h"
+
+using namespace std;
+ 
+int main()
+{
+    setlocale(LC_ALL, "");
+ 
+    cout << "Введите радиус и высоту конуса: ";
+    double r, h;
+    cin >> r >> h;
+    Cone c1(r, h);
+
+
+
+    cout << "Введите координаты центра основания, радиус и высоту конуса: ";
+    double x, y, z;
+    cin >> x >> y >> z >> r >> h;
+    Cone c2(x, y, z, r, h);
+
+    
+    cout << endl << "Конус 1 " << endl;
+    c1.print();
+    cout << "Площадь полной поверхности: " << c1.area() << endl;
+    cout << "Объем: " << c1.volume() << endl;
+
+
+    cout << endl << "Конус 2 " << endl;
+    c2.print();
+    cout << "Площадь полной поверхности: " << c2.area() << endl;
+    cout << "Объем: " << c2.volume() << endl;
+
+
+
+    Cone* c3 = new Cone();
+    cout << endl << "Произвольный конус " << endl;
+    cout << "Введите координаты центра основания, радиус и высоту конуса: " << endl;
+    cin >> x >> y >> z >> r >> h;
+    c3->setCoordinate(x, y, z);
+    c3->setRadius(r);
+    c3->setHeight(h);
+    c3->print();
+    delete c3;
+
+
+
+    int n = 3;
+    Cone* mas = new Cone[n];
+    for (int i = 0; i < n; i++) {
+        cout << endl << "Введите координаты центра основания, радиус и высоту " << i+1 << " конуса: " << endl;
+		cin >> x >> y >> z >> r >> h;
+		mas[i].setCoordinate(x, y, z);
+		mas[i].setRadius(r);
+		mas[i].setHeight(h);
+	}
+
+    for (int i = 0; i < n; i++) {
+		cout << endl << "Конус " << i + 1 << endl;
+		mas[i].print();
+		cout << "Площадь полной поверхности: " << mas[i].area() << endl;
+		cout << "Объем: " << mas[i].volume() << endl;
+	}
+
+    delete[] mas;
+
+    return 0;
+}
---
+++ week 6/Ex 2/Задача 2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+#include <windows.h>
+#include "Cone.h"
+#include "Truncated_Cone.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	Cone c1(1, 2, 3, 4, 5);
+	Truncated_Cone c2(1, 2, 3, 4, 5, 6);
+	cout << "Площадь полной поверхности конуса: " << c1.area() << endl;
+	cout << "Объем конуса: " << c1.volume() << endl;
+	cout << "Площадь полной поверхности усеченного конуса: " << c2.area() << endl;
+	cout << "Объем усеченного конуса: " << c2.volume() << endl;
+	cout << endl;
+
+
+
+	Cone c3;
+	cout << "Введите координаты центра (x,y,z): ";
+	double x, y, z;
+	cin >> x >> y >> z;
+	c3.setCoordinate(x, y, z);
+	cout << "Введите радиус и высоту конуса: ";
+	double r, h;
+	cin >> r >> h;
+	c3.setRadius(r);
+	c3.setHeight(h);
+	cout << endl;
+	cout << " >> Фигура 3" << endl;
+	c3.print();
+	cout << endl;
+
+	Truncated_Cone c4(x, y, z, r, 3, h);
+	Truncated_Cone c5(x, y, z, r, 4, h);
+	Truncated_Cone c6(x, y, z, r, 5, h);
+	cout << " >> Фигура 4" << c4 << endl;
+	cout << " >> Фигура 5" << c5 << endl;
+	cout << " >> Фигура 6" << c6 << endl;
+	cout << endl;
+
+	// Проверка операторов сравнения
+	cout << "c2 != c6: " << (c2 != c6 ? "Да" : "Нет") << endl;
+	cout << "c2 > c6 : " << (c2 > c6 ? "Да" : "Нет") << endl;
+	cout << "c2 < c6 : " << (c2 < c6 ? "Да" : "Нет") << endl;
+	cout << "c2 >= c6: " << (c2 >= c6 ? "Да" : "Нет") << endl;
+	cout << "c2 <= c6: " << (c2 <= c6 ? "Да" : "Нет") << endl;
+
+	return 0;
+}
